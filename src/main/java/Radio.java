65e34127a7c41bab5f43999d1e67a439984c812f
Serg75 <activequest@gmail.com>
2026-01-21 15:33:42 +0300

    changes

---
+++ src/main/java/Radio.java
@@ -0,0 +1,69 @@
+public class Radio {
+
+    public int currentVolume;
+
+    public int currentStation;
+
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume += 1;
+        } else {
+            currentVolume = 0;
+        }
+
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume -= 1;
+        } else {
+            currentVolume = 100;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation > 9) {
+            return;
+        }
+        if (newCurrentStation < 0) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void next() {
+        if (currentStation < 9) {
+            currentStation += 1;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStation > 0) {
+            currentStation -= 1;
+        } else {
+            currentStation = 9;
+        }
+    }
+}
+
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,160 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    Radio more = new Radio();
+
+    @Test
+    public void shouldVolumeMore() {
+
+        more.setCurrentVolume(95);
+
+        more.increaseVolume();
+
+        int expected = 96;
+        int actual = more.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldVolumeMore1() {
+
+        more.setCurrentVolume(100);
+
+        more.increaseVolume();
+
+        int expected = 0;
+        int actual = more.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldVolumeLess() {
+
+        more.setCurrentVolume(95);
+
+        more.decreaseVolume();
+
+        int expected = 94;
+        int actual = more.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldVolumeLess1() {
+
+        more.setCurrentVolume(0);
+
+        more.decreaseVolume();
+
+        int expected = 100;
+        int actual = more.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldVolumeExceed() {
+
+        more.setCurrentVolume(101);
+
+        int expected = 0;
+        int actual = more.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldVolumeExceed1() {
+
+        more.setCurrentVolume(-1);
+
+        int expected = 0;
+        int actual = more.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldStationMore() {
+
+        more.setCurrentStation(8);
+
+        more.next();
+
+        int expected = 9;
+        int actual = more.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldStationMore1() {
+
+        more.setCurrentStation(9);
+
+        more.next();
+
+        int expected = 0;
+        int actual = more.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldStationLess() {
+
+        more.setCurrentStation(2);
+
+        more.prev();
+
+        int expected = 1;
+        int actual = more.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldStationLess1() {
+
+        more.setCurrentStation(0);
+
+        more.prev();
+
+        int expected = 9;
+        int actual = more.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldStationExceed() {
+
+        more.setCurrentStation(10);
+
+        int expected = 0;
+        int actual = more.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldStationExceed1() {
+
+        more.setCurrentStation(-1);
+
+        int expected = 0;
+        int actual = more.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
